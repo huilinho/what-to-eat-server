a8ff0e3b3b66c9def3c54012e5a59e9fb3575dbf
huilin <1647529134@qq.com>
2020-11-06 10:27:09 +0800

    fix:更新数据库sql语句

--- what-to-eat-server/doc/sql/db.sql
+++ what-to-eat-server/doc/sql/db.sql
@@ -1,3 +1,8 @@
+# Host: 127.0.0.1  (Version: 5.7.11-log)
+# Date: 2020-11-06 10:14:52
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES utf8 */;
 
 #
 # Structure for table "admin"
@@ -16,11 +21,12 @@ CREATE TABLE `admin` (
   `data_status` tinyint(2) NOT NULL DEFAULT '2' COMMENT '通用状态,2正常,3删除',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_mobile` (`mobile`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='管理员表';
 
 #
 # Data for table "admin"
 #
+
 INSERT INTO `admin` VALUES (1,'13381176580','e10adc3949ba59abbe56e057f20f883e','管理员','admin','','2020-11-02 00:34:13','2020-11-02 00:34:13',2),(2,'13967541234','e10adc3949ba59abbe56e057f20f883e','admin1','管理员1','','2020-11-02 21:42:12','2020-11-02 21:42:12',2);
 
 #
@@ -31,15 +37,31 @@ DROP TABLE IF EXISTS `appraisal`;
 CREATE TABLE `appraisal` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户id',
-  `dishes_id` int(10) NOT NULL DEFAULT '0' COMMENT '订单id',
-  `appraisal_grade` int(10) NOT NULL DEFAULT '3' COMMENT '评价等级，1.超难吃，2.难吃，3.一般，4.好吃，5.超好吃',
+  `dishes_id` int(10) NOT NULL DEFAULT '0' COMMENT '菜式id',
   `appraisal` text NOT NULL COMMENT '评价',
-  `creatTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评价表';
 
 #
 # Data for table "appraisal"
+#
+
+
+#
+# Structure for table "collection"
+#
+
+DROP TABLE IF EXISTS `collection`;
+CREATE TABLE `collection` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL DEFAULT '0',
+  `dishes_id` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "collection"
 #
 
 
@@ -53,10 +75,10 @@ CREATE TABLE `dishes` (
   `name` varchar(30) NOT NULL DEFAULT '0' COMMENT '菜式名',
   `window_id` int(10) NOT NULL DEFAULT '0' COMMENT '所属窗口id',
   `type_id` int(10) NOT NULL DEFAULT '0' COMMENT '种类id',
-  `comment_id` varchar(2550) NOT NULL DEFAULT '0' COMMENT '该菜式对应的所有评价id',
-  `score` decimal(5,2) NOT NULL COMMENT '星级分',
   `cover` varchar(1000) NOT NULL DEFAULT '' COMMENT '菜式图片路径',
-  `creatTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `like` int(10) DEFAULT '0' COMMENT '点赞数',
+  `hate` int(10) NOT NULL DEFAULT '0' COMMENT '踩数',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜式表';
 
@@ -91,7 +113,6 @@ CREATE TABLE `user` (
   `openid` varchar(64) NOT NULL DEFAULT '' COMMENT '微信openid',
   `username` varchar(32) NOT NULL DEFAULT '' COMMENT '微信昵称',
   `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
-  `collections` varchar(300) NOT NULL DEFAULT '' COMMENT '收藏菜式的id集合(字符串形式存储)',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `data_status` tinyint(2) NOT NULL DEFAULT '2' COMMENT '通用状态,2正常,3删除',
   PRIMARY KEY (`id`),
